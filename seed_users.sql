9f7f78d6141aac8bc807512b114255d4ff8478d2
Kunal Dabhane <kunaldabhane20012@gmail.com>
2026-01-30 21:49:59 +0530

    feat: Add initial seed data for users and roles, and update Sybase database connection environment variables.

---
+++ seed_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO Users (LoginId, Id, Name, Rank, Department, DateTimeJoined, StationCode) VALUES ('user1', 'ID5258', 'Sarah Brown', 'CAPTAIN', 'LOG', '2024-10-21 21:46:07', 'D');
+INSERT INTO UserRole (LoginId, RoleName, DateTimeActivated, StationCode) VALUES ('user1', 'NLAO', '2024-10-22 21:46:07', 'D');
+INSERT INTO UserRole (LoginId, RoleName, DateTimeActivated, StationCode) VALUES ('user1', 'AUDITOR', '2024-10-22 21:46:07', 'D');
+INSERT INTO Users (LoginId, Id, Name, Rank, Department, DateTimeJoined, StationCode) VALUES ('user2', 'ID7536', 'John Brown', 'CAPTAIN', 'ADMIN', '2025-08-27 21:46:07', 'A');
+INSERT INTO UserRole (LoginId, RoleName, DateTimeActivated, StationCode) VALUES ('user2', 'AUDITOR', '2025-08-28 21:46:07', 'A');
+INSERT INTO UserRole (LoginId, RoleName, DateTimeActivated, StationCode) VALUES ('user2', 'NLAO', '2025-08-28 21:46:07', 'A');
+INSERT INTO Users (LoginId, Id, Name, Rank, Department, DateTimeJoined, StationCode) VALUES ('user3', 'ID8827', 'Michael Jones', 'MAJOR', 'LOG', '2025-01-04 21:46:07', 'K');
+INSERT INTO UserRole (LoginId, RoleName, DateTimeActivated, StationCode) VALUES ('user3', 'AUDITOR', '2025-01-05 21:46:07', 'K');
