d92ed0289165660781c629652c93076c712b6051
Kunal Dabhane <kunaldabhane20012@gmail.com>
2026-01-31 17:51:46 +0530

    feat: add initial data for the Correspondence table

---
+++ sql_scripts/correspondence_table.sql
@@ -0,0 +1,37 @@
+
+
+INSERT INTO Correspondence VALUES
+(1,'WOSMaster','24','LOGO','t2533104','NLAO',
+ '2025-12-22 15:44:07','Fwded','K',
+ 'WOS forwarded by LOGO to NLAO',
+ 'NOTE',NULL,'Y')
+
+INSERT INTO Correspondence VALUES
+(2,'WOSMaster','24','NLAO','nlao01','LOGO',
+ '2026-01-29 10:57:38','Clarfy','K',
+ 'Clarification requested by NLAO',
+ 'NOTE',NULL,'Y')
+
+INSERT INTO Correspondence VALUES
+(3,'WOSMaster','24','LOGO','t2533104','NLAO',
+ '2026-01-29 14:12:10','Commt','K',
+ 'Clarification provided by LOGO',
+ 'NOTE',NULL,'Y')
+
+INSERT INTO Correspondence VALUES
+(4,'WOSMaster','24','NLAO','nlao01','LOGO',
+ '2026-01-29 15:30:22','Concur','K',
+ 'Concurrence recorded by NLAO',
+ 'NOTE',NULL,'Y')
+
+INSERT INTO Correspondence VALUES
+(5,'WOSMaster','24','LOGO','w03221','NLAO',
+ '2026-01-29 16:47:35','Appvd','K',
+ 'Approved and sent back to NLAO',
+ 'ORDER',NULL,'Y')
+
+INSERT INTO Correspondence VALUES
+(6,'WOSMaster','24','NLAO','nlao01','LOGO',
+ '2026-01-29 17:38:26','Rec','K',
+ 'Recommendation completed',
+ 'REC_NOTE',NULL,'Y')
